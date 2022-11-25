3f75d60e40aaaaf0bfd83409f33dceaddf70e5cf
tarekMedhat <46792171+TarekMedhat55@users.noreply.github.com>
2022-11-25 16:22:43 +0200

    sdad

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   //*Set static folder up in production
   app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("/*", function (req, res) {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
