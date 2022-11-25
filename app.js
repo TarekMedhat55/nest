e2a621a0989714f9606bca182943488af30315e4
tarekMedhat <46792171+TarekMedhat55@users.noreply.github.com>
2022-11-25 13:48:08 +0200

    vdsdv

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ if (process.env.NODE_ENV === "production") {
   //*Set static folder up in production
   app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("/*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  app.get("/*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
 }
 
 app.use(NotFoundMiddleware);
